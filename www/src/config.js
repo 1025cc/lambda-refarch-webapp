3ff913328ba2b6775d263d0088c1b8887628c3f5
cccccci <709313510@qq.com>
2023-05-09 23:20:08 +0800

    Update frontend config

--- www/src/config.js
+++ www/src/config.js
@@ -2,10 +2,10 @@
 // aws cloudformation describe-stacks --stack-name <YOUR STACK NAME> --query "Stacks[0].Outputs[]"
 
 const config = {
-  "aws_user_pools_web_client_id": "",     // CognitoClientID
-  "api_base_url": "",                                     // TodoFunctionApi
-  "cognito_hosted_domain": "",                   // CognitoDomainName
-  "redirect_url": ""                                      // AmplifyURL
+  "aws_user_pools_web_client_id": "70n8unmaqitnvru4un3eukalea",     // CognitoClientID
+  "api_base_url": "https://5bpfoklt25.execute-api.us-east-1.amazonaws.com/{StageNameParam}",                                     // TodoFunctionApi
+  "cognito_hosted_domain": "mytodoappdemo-stack2.auth.us-east-1.amazoncognito.com",                   // CognitoDomainName
+  "redirect_url": "https://master.d1a9qsoy01toy.amplifyapp.com"                                      // AmplifyURL
 };
 
 export default config;
